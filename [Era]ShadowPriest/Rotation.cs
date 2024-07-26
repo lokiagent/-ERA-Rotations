fd91677cc706ab10b3a28e7dedee413cb868a900
SpeedySaky <31747079+SpeedySaky@users.noreply.github.com>
2024-07-26 20:06:44 +0200

    update

--- [Era]ShadowPriest/Rotation.cs
+++ [Era]ShadowPriest/Rotation.cs
@@ -6,20 +6,8 @@ using wShadow.Warcraft.Classes;
 using wShadow.Warcraft.Defines;
 using wShadow.Warcraft.Managers;
 
-
-public class PriestShadow : Rotation
+public class EraShadowPriest : Rotation
 {
-
-    private int debugInterval = 5; // Set the debug interval in seconds
-    private DateTime lastDebugTime = DateTime.MinValue;
-    private DateTime lastHands = DateTime.MinValue;
-    private TimeSpan Hands = TimeSpan.FromSeconds(12);
-    private DateTime lastPants = DateTime.MinValue;
-    private TimeSpan Pants = TimeSpan.FromSeconds(120);
-    private DateTime lastChest = DateTime.MinValue;
-    private TimeSpan Chest = TimeSpan.FromSeconds(8);
-
-
     private List<string> npcConditions = new List<string>
     {
         "Innkeeper", "Auctioneer", "Banker", "FlightMaster", "GuildBanker",
@@ -37,23 +25,29 @@ public class PriestShadow : Rotation
         return true;
     }
     private bool HasItem(object item) => Api.Inventory.HasItem(item);
+
+    private DateTime lastDebugTime = DateTime.MinValue;
+    private int debugInterval = 5; // Set the debug interval in seconds
+
     private bool HasEnchantment(EquipmentSlot slot, string enchantmentName)
     {
         return Api.Equipment.HasEnchantment(slot, enchantmentName);
     }
 
 
+
+
     public override void Initialize()
     {
         // Can set min/max levels required for this rotation.
 
-        lastDebugTime = DateTime.Now;
+
         LogPlayerStats();
         // Use this method to set your tick speeds.
         // The simplest calculation for optimal ticks (to avoid key spam and false attempts)
 
         // Assuming wShadow is an instance of some class containing UnitRatings property
-        SlowTick = 1550;
+        SlowTick = 750;
         FastTick = 500;
 
         // You can also use this method to add to various action lists.
@@ -76,6 +70,7 @@ public class PriestShadow : Rotation
         var me = Api.Player;
         var mana = me.ManaPercent;
         var target = Api.Target;
+        var targethealth = target.HealthPercent;
 
         if ((DateTime.Now - lastDebugTime).TotalSeconds >= debugInterval)
         {
@@ -89,77 +84,121 @@ public class PriestShadow : Rotation
 
         // Target distance from the player
 
-        if (me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving() || me.IsChanneling() || me.IsLooting() || me.IsMounted() || me.Auras.Contains("Drink") || me.Auras.Contains("Food")) return false;
-        if (me.IsValid())
+        if (me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving() || me.IsChanneling() || me.IsMounted() || me.Auras.Contains("Drink") || me.Auras.Contains("Food"))
         {
-            if (Api.Spellbook.CanCast("Renew") && !me.Auras.Contains("Renew") && healthPercentage < 80)
+            return false;
+        }
+
+        if (Api.Spellbook.CanCast("Renew") && !me.Auras.Contains("Renew") && healthPercentage < 80)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Renew");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Renew"))
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Casting Renew");
-                Console.ResetColor();
-                if (Api.Spellbook.Cast("Renew"))
-                {
-                    return true;
-                }
+                return true;
             }
-            if (Api.Spellbook.CanCast("Power Word: Fortitude") && !me.Auras.Contains("Power Word: Fortitude"))
+        }
+        if (Api.Spellbook.CanCast("Power Word: Fortitude") && !me.Auras.Contains("Power Word: Fortitude"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Power Word: Fortitude");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Power Word: Fortitude"))
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Casting Power Word: Fortitude");
-                Console.ResetColor();
-                if (Api.Spellbook.Cast("Power Word: Fortitude"))
-                {
-                    return true;
-                }
+                return true;
             }
-            if (Api.Spellbook.CanCast("Inner Fire") && !me.Auras.Contains("Inner Fire"))
+        }
+        if (Api.Spellbook.CanCast("Shadow Protection") && !me.Auras.Contains("Shadow Protection"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Shadow Protection");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Power Shadow Protection"))
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Casting Inner Fire");
-                Console.ResetColor();
-                if (Api.Spellbook.Cast("Inner Fire"))
-                {
-                    return true;
-                }
+                return true;
             }
-            if (Api.Spellbook.CanCast("Shadowform") && !me.Auras.Contains("Shadowform", false) && mana > 80)
+        }
+        if (Api.Spellbook.CanCast("Inner Fire") && !me.Auras.Contains("Inner Fire"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Inner Fire");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Inner Fire"))
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Casting Shadowform");
-                Console.ResetColor();
-                if (Api.Spellbook.Cast("Shadowform"))
-                {
-                    return true;
-                }
+                return true;
+            }
+        }
+        if (Api.Spellbook.CanCast("Power Word: Shield") && !me.Auras.Contains("Power Word: Shield") && me.InCombat())
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Power Word: Shield");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Power Word: Shield"))
+            {
+                return true;
+            }
+        }
+
+        if (!me.Auras.Contains("Shadowform", false) && Api.Spellbook.CanCast("Shadowform"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Shadowform");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Shadowform"))
+            {
+                return true;
             }
         }
         var reaction = me.GetReaction(target);
-
-
-        var reaction = me.GetReaction(target);
         if (target.IsValid())
         {
-            if (!target.IsDead() &&
-            (reaction != UnitReaction.Friendly &&
-             reaction != UnitReaction.Honored &&
-             reaction != UnitReaction.Revered &&
-             reaction != UnitReaction.Exalted) &&
-            mana > 20 && !IsNPC(target))
+            if (!target.IsDead())
             {
-                Console.WriteLine("Trying to cast Smite");
-
-                // Try casting Pyroblast
-                if (Api.Spellbook.CanCast("Smite"))
+                if (reaction != UnitReaction.Friendly && reaction != UnitReaction.Honored && reaction != UnitReaction.Revered && reaction != UnitReaction.Exalted)
                 {
-                    Api.Spellbook.Cast("Smite");
-                    Console.WriteLine("Casting Smite");
+                    if (mana >= 5)
                     {
-                        return true;
+                        if (!IsNPC(target))
+                        {
+                            if (Api.Spellbook.CanCast("Smite"))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Casting Smite");
+                                Console.ResetColor();
+                                if (Api.Spellbook.Cast("Smite"))
+                                {
+                                    return true;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Smite is not ready to be cast.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Target is an NPC.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mana is not above 20%.");
                     }
                 }
-
-                return base.PassivePulse();
+                else
+                {
+                    Console.WriteLine("Target is friendly, honored, revered, or exalted.");
+                }
             }
+            else
+            {
+                Console.WriteLine("Target is dead.");
+            }
+        }
+
+        return base.PassivePulse();
+    }
 
 
     public override bool CombatPulse()
@@ -167,7 +206,6 @@ public class PriestShadow : Rotation
         var me = Api.Player;
         var target = Api.Target;
         var mana = me.ManaPercent;
-        if (!me.IsValid() || !target.IsValid() || me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving() || me.IsChanneling() || me.IsMounted() || me.Auras.Contains("Drink") || me.Auras.Contains("Food")) return false;
 
         if ((DateTime.Now - lastDebugTime).TotalSeconds >= debugInterval)
         {
@@ -177,10 +215,11 @@ public class PriestShadow : Rotation
         // Health percentage of the player
         var healthPercentage = me.HealthPercent;
         var targethealth = target.HealthPercent;
-        if (me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving() || me.IsChanneling() || me.IsMounted() || me.Auras.Contains("Drink") || me.Auras.Contains("Food")) return false;
+        if (!me.IsValid() || !target.IsValid() || me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving() || me.IsChanneling() || me.IsMounted() || me.Auras.Contains("Drink") || me.Auras.Contains("Food")) return false;
 
         string[] HP = { "Major Healing Potion", "Superior Healing Potion", "Greater Healing Potion", "Healing Potion", "Lesser Healing Potion", "Minor Healing Potion" };
         string[] MP = { "Major Mana Potion", "Superior Mana Potion", "Greater Mana Potion", "Mana Potion", "Lesser Mana Potion", "Minor Mana Potion" };
+
 
         if (me.HealthPercent <= 70 && (!Api.Inventory.OnCooldown(MP) || !Api.Inventory.OnCooldown(HP)))
         {
@@ -228,113 +267,17 @@ public class PriestShadow : Rotation
                 return true;
             }
         }
-
-        if (Api.Spellbook.CanCast("Renew") && !me.Auras.Contains("Renew") && healthPercentage < 80)
+        if (Api.Spellbook.CanCast("Silence") && !Api.Spellbook.OnCooldown("Silence") && (target.IsCasting() || target.IsChanneling()))
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Casting Renew");
+            Console.WriteLine("Casting Silence");
             Console.ResetColor();
-            if (Api.Spellbook.Cast("Renew"))
+            if (Api.Spellbook.Cast("Silence"))
             {
                 return true;
             }
         }
-
-        if (Api.Spellbook.CanCast("Desperate Prayer") && healthPercentage < 50)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Desperate Prayer");
-            Console.ResetColor();
-            if (Api.Spellbook.Cast("Desperate Prayer"))
-            {
-                return true;
-            }
-        }
-
-
-
-        if (Api.HasMacro("Hands"))
-        {
-            bool hasEnchantmentOnHands = HasEnchantment(EquipmentSlot.Hands, "Penance");
-
-            if (hasEnchantmentOnHands)
-            {
-                TimeSpan timeSinceLastCast = DateTime.Now - lastHands;
-                if (timeSinceLastCast >= Hands)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Casting Hands rune");
-                    Console.ResetColor();
-
-                    if (Api.UseMacro("Hands"))
-                    {
-                        lastHands = DateTime.Now;
-                        Console.WriteLine($"Hands rune cast at {lastHands}. Next cast available after {lastHands + Hands}.");
-                        return true;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Hands rune is on cooldown. {Hands - timeSinceLastCast} remaining. Skipping cast.");
-                }
-            }
-        }
-
-        if (Api.HasMacro("Chest"))
-        {
-            bool hasEnchantmentOnChest = HasEnchantment(EquipmentSlot.Chest, "Void Plague");
-
-            if (hasEnchantmentOnChest)
-            {
-                TimeSpan timeSinceLastCast = DateTime.Now - lastChest;
-                if (timeSinceLastCast >= Chest)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Casting Chest rune");
-                    Console.ResetColor();
-
-                    if (Api.UseMacro("Chest"))
-                    {
-                        lastChest = DateTime.Now;
-                        Console.WriteLine($"Chest rune cast at {lastChest}. Next cast available after {lastChest + Chest}.");
-                        return true;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Chest rune is on cooldown. {Chest - timeSinceLastCast} remaining. Skipping cast.");
-                }
-            }
-        }
-
-        if (Api.HasMacro("Legs"))
-        {
-            bool hasEnchantmentOnLegs = HasEnchantment(EquipmentSlot.Legs, "Homunculi");
-
-            if (hasEnchantmentOnLegs)
-            {
-                TimeSpan timeSinceLastCast = DateTime.Now - lastPants;
-                if (timeSinceLastCast >= Pants)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Casting Legs rune");
-                    Console.ResetColor();
-
-                    if (Api.UseMacro("Legs"))
-                    {
-                        lastPants = DateTime.Now;
-                        Console.WriteLine($"Legs rune cast at {lastPants}. Next cast available after {lastPants + Pants}.");
-                        return true;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Legs rune is on cooldown. {Pants - timeSinceLastCast} remaining. Skipping cast.");
-                }
-            }
-        }
-
-        if (Api.Spellbook.CanCast("Shadow Word: Pain") && !target.Auras.Contains(589) && mana > 30)
+        if (Api.Spellbook.CanCast("Shadow Word: Pain") && !target.Auras.Contains("Shadow Word: Pain") && targethealth >= 30 && mana > 10)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Casting Shadow Word: Pain");
@@ -344,8 +287,17 @@ public class PriestShadow : Rotation
                 return true;
             }
         }
-
-        if (Api.Spellbook.CanCast("Mind Blast") && targethealth >= 30 && mana > 10)
+        if (Api.Spellbook.CanCast("Vampiric Embrace") && !target.Auras.Contains("Vampiric Embrace") && targethealth >= 30 && mana > 10)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Vampiric Embrace");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Vampiric Embrace"))
+            {
+                return true;
+            }
+        }
+        if (Api.Spellbook.CanCast("Mind Blast") && targethealth >= 30 && mana > 10 && !Api.Spellbook.OnCooldown("Mind Blast"))
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Casting Mind Blast");
@@ -356,23 +308,27 @@ public class PriestShadow : Rotation
             }
         }
 
-        if (Api.Equipment.HasItem(EquipmentSlot.Extra))
+        if (Api.Spellbook.CanCast("Smite") && mana > 10 && !me.Auras.Contains("Shadowform", false))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Casting Smite");
+            Console.ResetColor();
+            if (Api.Spellbook.Cast("Smite"))
+
+                return true;
+        }
+        if (Api.Equipment.HasItem(EquipmentSlot.Extra) && Api.HasMacro("Shoot"))
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Ranged weapon is equipped. Attempting to cast Shoot.");
             Console.ResetColor();
 
-            if (Api.HasMacro("Shoot") && Api.UseMacro("Shoot"))
+            if (Api.UseMacro("Shoot"))
             {
                 return true;
             }
         }
-        else
-        {
-            Console.WriteLine("No ranged weapon equipped. Skipping Shoot.");
-        }
-
-        if (Api.Spellbook.CanCast("Attack") && mana <= 2)
+        if (Api.Spellbook.CanCast("Attack"))
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Casting Attack");
@@ -383,8 +339,10 @@ public class PriestShadow : Rotation
             }
         }
 
+
         return base.CombatPulse();
     }
+
 
     private bool IsNPC(WowUnit unit)
     {
@@ -420,14 +378,29 @@ public class PriestShadow : Rotation
         var mana = me.ManaPercent;
         var healthPercentage = me.HealthPercent;
 
-
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"{mana}% Mana available");
         Console.WriteLine($"{healthPercentage}% Health available");
         Console.ResetColor();
+        if (Api.HasMacro("Shoot"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wand macro 'Shoot' is present.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("INGAME ..... Create Macro ");
+            Console.WriteLine("Macro name : Shoot");
+            Console.WriteLine("Macro code : /cast !Shoot");
 
-
-
+            Console.WriteLine("Save macro, exit options and when ingame RELOAD UI");
+            Console.ResetColor();
+        }
     }
+
+
+
 
 }
